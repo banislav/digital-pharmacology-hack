732526769f88283b014482747a9df559cf75a581
sexozavr <daniildiveev@mail.ru>
2023-04-28 20:13:45 +0300

    [MODIFY] add banik functions to utils/parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,5 +1,6 @@
 import re
 import urllib.parse
+from typing import Dict
 from time import sleep
 from typing import List
 
@@ -7,7 +8,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-TIME_TO_LOAD_PAGE = 5
+TIME_TO_LOAD_PAGE = 3
 SOURCE_URL = "https://pubchem.ncbi.nlm.nih.gov/"
 
 
@@ -38,3 +39,40 @@ def get_search_page(query: str) -> List[str]:
             compounds_links.append(match.group())
 
     return compounds_links
+
+
+def get_page_content(url: str) -> str:
+    with webdriver.Chrome(ChromeDriverManager().install()) as driver:
+        driver.get(url)
+        sleep(TIME_TO_LOAD_PAGE)
+        content = driver.page_source
+
+    return content
+
+
+def find_toxicity(content: str) -> Dict[str, Dict[str, int]]:
+    toxicity_dict = {'Human-Toxicity': {}, 'Non-Human-Toxicity': {}}
+
+    soup = BeautifulSoup(content, features='lxml')
+    human_tv = soup.find_all('section', attrs={'id': 'Human-Toxicity-Values'})
+    nonhuman_tv = soup.find_all('section', attrs={'id': 'Non-Human-Toxicity-Values'})
+
+    human_tv = [ht.find_all('div', attrs={'class': 'section-content-item'}) for ht in human_tv]
+    human_tv = [ht.find_all('p')[0].text for ht in human_tv[0]]
+
+    nonhuman_tv = [nht.find_all('div', attrs={'class': 'section-content-item'}) for nht in nonhuman_tv]
+    nonhuman_tv = [ht.find_all('p')[0].text for ht in nonhuman_tv[0]]
+
+    for tv in human_tv:
+        match = re.findall(r'([A-Z][A-Z]50 \D*)\s+(\d+)\s+[M|m][G|g]/[K|k][G|g]', tv)
+        if match:
+            key, val = match[0]
+            toxicity_dict['Human-Toxicity'][key] = int(val)
+
+    for tv in nonhuman_tv:
+        match = re.findall(r'([A-Z][A-Z]50 \D*)\s+(\d+)\s+[M|m][G|g]/[K|k][G|g]', tv)
+        if match:
+            key, val = match[0]
+            toxicity_dict['Non-Human-Toxicity'][key] = int(val)
+
+    return toxicity_dict
